fb348c1f4929af10f7d1ed1c531b7cb833cdd473
BeignetPA <22002963@etu.unicaen.fr>
2022-02-28 14:24:51 +0100

    Passage de variables à la vue via Repository. Plus de "compact('users', ...)"

--- app/controllers/OrgaController.php
+++ app/controllers/OrgaController.php
@@ -21,15 +21,15 @@ class OrgaController extends \controllers\ControllerBase{
 
      #[Get(name: 'orga.index')]
 	public function index(){
-        $orgas = DAO::getAll(Organization::class);
-		$this->loadView("OrgaController/index.html", compact('orgas'));
+        $orgas = $this->repo->all();
+		$this->loadView("OrgaController/index.html");
 	}
 
 	#[Route(path: "/getOne/{idOrga}",name: "orga.getOne")]
 	public function getOne($idOrga){
         //$orga = DAO::getById(Organization::class, $idOrga, ['users', 'groupes']);
         $orga=$this->repo->byId($idOrga,['users','groupes']);
-        $this->loadView("OrgaController/getOne.html", compact('orga'));
+        $this->loadView("OrgaController/getOne.html");
 	}
 
 
